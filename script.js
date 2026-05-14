9c915e66292148e00e045a94e936565c6af34928
prathameshbhagat <prathameshbhagat0608@gmail.com>
2026-05-14 10:54:11 +0530

    Bounce on x instead ...

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const t  = gsap.timeline({defaults:{ }});
+
 for(var i =0 ; i <5; i++){
-t.to('.box',{y : '-100%'})
-t.to('.box',{y : '100%', ease: 'bounce'})
+    t.to('.box',{x : '-100%', ease: 'bounce'})
+    t.to('.box',{x : '100%', ease: 'bounce'})
 }
-//t.to('.box',{ borderRadius:0})
-t.to('.box',{y : '-10%'})
-//t.to('.box',{ borderRadius:50})
+t.to('.box',{x : '00%', ease: 'bounce'})
 t.to('.box',{ width : '500px'})
