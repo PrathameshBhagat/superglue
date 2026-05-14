4d93309dadcb840ac599b048cd13a7987e64cc08
prathameshbhagat <prathameshbhagat0608@gmail.com>
2026-05-14 10:52:09 +0530

    bounce added

--- script.js
+++ script.js
@@ -1,18 +1,8 @@
 const t  = gsap.timeline({defaults:{ }});
-
-t.to('.box',{y : '100%'})
+for(var i =0 ; i <5; i++){
 t.to('.box',{y : '-100%'})
-t.to('.box',{y : '100%'})
-//t.to('.box',{ borderRadius:0})
-t.to('.box',{y : '-100%'})
-//t.to('.box',{ borderRadius:50})
-t.to('.box',{y : '100%'})
-t.to('.box',{y : '-100%'})
-//t.to('.box',{ borderRadius:0})
-t.to('.box',{y : '100%'})
-//t.to('.box',{ borderRadius:50})
-t.to('.box',{y : '-100%'})
-t.to('.box',{y : '100%'})
+t.to('.box',{y : '100%', ease: 'bounce'})
+}
 //t.to('.box',{ borderRadius:0})
 t.to('.box',{y : '-10%'})
 //t.to('.box',{ borderRadius:50})
