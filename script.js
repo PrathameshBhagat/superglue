a78641afb0da399a707146462615354294cd6dd5
prathameshbhagat <prathameshbhagat0608@gmail.com>
2026-05-14 11:27:28 +0530

    Everything (most things) works greate!

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const t  = gsap.timeline({defaults:{ }});
 
+t.to('.text',{  display: 'none'})
 for(var i =0 ; i <5; i++){
     t.to('.box',{x : '-100%', ease: 'bounce'})
     t.to('.box',{x : '100%', ease: 'bounce'})
 }
 t.to('.box',{x : '00%', ease: 'bounce'})
 t.to('.box',{ width : '500px'})
+t.to('#logo-text',{ opacity : 1})
+t.to('.box',{ backgroundColor : "white"});
+t.to('.box',{  border: "5px solid black" })
+t.to('.box',{ color:"black"})
+t.to('.box',{ x:"-500"})
+t.to('.box',{ y:"-200"})
+t.to('.box',{ x:"00"})
+t.to('.text',{ display: 'block'})
+t.to('.text',{ opacity : 1})
